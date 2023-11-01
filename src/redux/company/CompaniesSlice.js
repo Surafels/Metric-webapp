0b9f00af7d93170d17d1ce006000303bd0d1f70c
Surafels <ssurafel4@gmail.com>
2023-11-01 14:06:23 +0300

    Add initial state

--- src/redux/company/CompaniesSlice.js
+++ src/redux/company/CompaniesSlice.js
@@ -1,2 +1,16 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+
+const apiKey = '66bccab5c51dff5ab45457f9fa2d1bea';
+const url = `https://financialmodelingprep.com/api/v3/search?query=AA&apikey=${apiKey}`;
+
+const initialState = {
+  companies: [],
+  companyDetails: [],
+  isLoading: false,
+  error: false,
+};
+
+const companiesData = createAsyncThunk('companies/companiesData', async()=>{
+    
+})
