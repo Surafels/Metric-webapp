26928b6f69207b88dc635ebec4494bb2c58fa74e
Surafels <ssurafel4@gmail.com>
2023-11-04 12:26:32 +0300

    Add test for redux code

---
+++ src/tests/CompanyDetail.test.js
@@ -0,0 +1,38 @@
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { companiesData } from '../redux/company/CompaniesSlice';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe('companiesSlice actions and reducers', () => {
+  it('should dispatch companiesData.fulfilled action with the correct payload', async () => {
+    const initialState = {
+      companies: [],
+      companyDetails: [],
+      isLoading: false,
+      error: false,
+    };
+
+    const store = mockStore(initialState);
+
+    await store.dispatch(companiesData());
+
+    expect(store.getActions()).toMatchSnapshot();
+  });
+
+  it('should dispatch companiesData.rejected action with the correct error message', async () => {
+    const initialState = {
+      companies: [],
+      companyDetails: [],
+      isLoading: false,
+      error: false,
+    };
+
+    const store = mockStore(initialState);
+
+    await store.dispatch(companiesData());
+
+    expect(store.getActions()).toMatchSnapshot();
+  });
+});
